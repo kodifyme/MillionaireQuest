07cd3c48c3c73873350cbdd4982efad403ea4529
Nikolay <koddek@bk.ru>
2024-03-13 21:49:25 +0300

    initial commit

---
+++ MillionaireQuest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MillionaireQuest
+//
+//  Created by KOДИ on 13.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
